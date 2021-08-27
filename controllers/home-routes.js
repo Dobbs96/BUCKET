58e3586cdcebb7f95efe1018a2aa4a6fbf02070a
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-08-27 12:26:21 -0400

    added signup and login forms and rendering their handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,4 +2,8 @@ const router = require("express").Router();
 
 router.get("/", (req, res) => res.render("home"));
 
+router.get("/login", (req, res) => res.render("login"));
+
+router.get("/signup", (req, res) => res.render("signup"));
+
 module.exports = router;
