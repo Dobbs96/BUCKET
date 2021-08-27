55ada6df5ae23e515865b4865e9c17ad4419ed88
Stephanie Blanchard <sblanchard2432@gmail.com>
2021-08-27 12:43:45 -0400

    added dashboard card

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => res.send("Hello Dashboard"));
+router.get("/", (req, res) => res.render("dashboard"));
 
 module.exports = router;
