7bc078188c5588eb03a38ff5c0bf95d36c2f4aad
Rachel Gregg <rgregg2014@gmail.com>
2021-08-26 20:25:18 -0400

    added views files, put in TODO comments to lay out what needs to be put on each page, finished global elements

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const models = require("../models");
 
-router.get("/", (req, res) => res.send("Hello there"));
+router.get("/", (req, res) => res.render("home"));
 
 module.exports = router;
--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,14 @@
 //====FORM CONTROLS MATERIALIZE CSS
+
 //====BUTTON CONTROLS MATERIALIZE CSS
+
 //====NAVBAR CONTROLS MATERIALIZE CSS
+
+const options = {
+  //fill this with how you want the menu to behave
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems, options);
+});
