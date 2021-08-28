e05d28791d1e5c89a4de722f17ce8c2fc28f002f
James Dobbs <dobbs282@gmail.com>
2021-08-28 15:20:24 -0400

    ALL COMMENTS CAN BE ADDED

--- public/js/newComment.js
+++ public/js/newComment.js
@@ -1,11 +1,10 @@
 function newComment() {
   var x = document.getElementById("comment").value;
   return (document.getElementById("new-comment").innerHTML = `<div class="row">
-  <form class="col s12">
-    <div class="row">
+      <div class="row">
       <div class="input-field col s12">
-        <textarea id="textarea1" class="materialize-textarea"></textarea>
-        <label for="textarea1">Textarea</label>
+        <textarea id="comment" name="comment" class="materialize-textarea"></textarea>
+        <label for="comment">Textarea</label>
       </div>
     </div>
     <button
@@ -15,6 +14,5 @@ function newComment() {
         >Submit
           <i class="material-icons right">send</i>
         </button>
-  </form>
 </div>`);
 }
