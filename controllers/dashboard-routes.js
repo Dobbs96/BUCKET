38b2d2f4282c9823ac7b028d12a9a893397bb43a
James Dobbs <dobbs282@gmail.com>
2021-08-28 10:26:36 -0400

    getting by user_id session

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,9 +1,40 @@
 const router = require("express").Router();
+const { Experience, Recipe, Travel, User, Comment } = require("../models");
 
 // router.get("/", (req, res) => res.send("Hello Dashboard"));
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const experienceData = await Experience.findAll({
+    where: {
+      user_id: req.session.userId,
+    },
+    include: [{ model: User, attributes: { exclude: ["password"] } }],
+  }).catch((err) => res.status(500).json(err));
+  const recipeData = await Recipe.findAll({
+    where: {
+      user_id: req.session.userId,
+    },
+    include: [{ model: User, attributes: { exclude: ["password"] } }],
+  }).catch((err) => res.status(500).json(err));
+  const travelData = await Travel.findAll({
+    where: {
+      user_id: req.session.userId,
+    },
+    include: [{ model: User, attributes: { exclude: ["password"] } }],
+  }).catch((err) => res.status(500).json(err));
+
+  const experiences = await experienceData.map((experience) =>
+    experience.get({ plain: true })
+  );
+  const recipes = await recipeData.map((recipe) => recipe.get({ plain: true }));
+  const travels = await travelData.map((travel) => travel.get({ plain: true }));
+
   console.log("inside dashboard-routes.js get / ");
-  res.render("dashboard", { loggedIn: req.session.loggedIn });
+  res.render("home", {
+    experiences,
+    recipes,
+    travels,
+    loggedIn: req.session.loggedIn,
+  });
 });
 
 module.exports = router;
