aba2808efd97cdc515e9429dbc4883391081b2fc
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-08-28 22:53:14 -0400

    added code for recipe and experience updates

--- public/js/updatePost.js
+++ public/js/updatePost.js
@@ -1,10 +1,4 @@
-let locationInfo;
-let budgetInfo;
-let what_doInfo;
-let starting_dateInfo;
-let ending_dateInfo;
-
-const submitPut = async (event) => {
+const updateTravelPost = async (event) => {
   event.preventDefault();
 
   const travelBtn = document.querySelector("#updateTrvlBtn");
@@ -35,22 +29,68 @@ const submitPut = async (event) => {
   }
 };
 
-const updateRecipeFormHandler = async (event) => {
+const updateRecipePost = async (event) => {
   event.preventDefault();
-  console.log("update recipe button clicked");
+
+  const recipeBtn = document.querySelector("#updateRecipeBtn");
+  recipeId = recipeBtn.getAttribute("data-id");
+
+  const name = document.querySelector("#name").value;
+  const price = document.querySelector("#price").value;
+  const link = document.querySelector("#link").value;
+
+  const response = await fetch("/api/recipe/" + recipeId, {
+    method: "PUT",
+    body: JSON.stringify({
+      name,
+      price,
+      link,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert("Failed to update Recipe information");
+  }
 };
 
-const updateExperienceFormHandler = async (event) => {
+const updateExpPost = async (event) => {
   event.preventDefault();
-  console.log("update experience button clicked");
+
+  const expBtn = document.querySelector("#updateExpBtn");
+  expId = expBtn.getAttribute("data-id");
+
+  const what_do = document.querySelector("#what-do").value;
+  const budget = document.querySelector("#budget").value;
+  const rating = document.querySelector("#rating").value;
+
+  const response = await fetch("/api/experience/" + expId, {
+    method: "PUT",
+    body: JSON.stringify({
+      what_do,
+      budget,
+      rating,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert("Failed to update Experience information");
+  }
 };
 
-document.querySelector("#updateTrvlBtn").addEventListener("click", submitPut);
+document
+  .querySelector("#updateTrvlBtn")
+  .addEventListener("click", updateTravelPost);
 
 document
-  .querySelector(".recipe-card-container")
-  .addEventListener("click", updateRecipeFormHandler);
+  .querySelector(".updateRecipeBtn")
+  .addEventListener("click", updateRecipePost);
 
 document
-  .querySelector(".experience-card-container")
-  .addEventListener("click", updateExperienceFormHandler);
+  .querySelector(".updateExpBtn")
+  .addEventListener("click", updateExpPost);
